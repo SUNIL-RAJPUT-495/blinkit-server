7a10034acc11023e3305c6075158cc43635d0ba8
SUNIL-RAJPUT-495 <sunilsinghshekhawat495@gmail.com>
2026-05-18 13:01:57 +0530

    updTEDD

--- api/index.js
+++ api/index.js
@@ -70,9 +70,11 @@ app.use("/api/product", product);
 app.use("/api/order",order)
 
 app.use("/api/address",address)
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
 
 export default app;
 
