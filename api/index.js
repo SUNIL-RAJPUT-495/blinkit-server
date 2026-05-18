156806796433a9892cabbf6ec6c936fd278c34d4
SUNIL-RAJPUT-495 <sunilsinghshekhawat495@gmail.com>
2026-05-18 12:58:29 +0530

    updated core

--- api/index.js
+++ api/index.js
@@ -24,11 +24,8 @@ import address from '../route/adminRouter/address.router.js';
 const app = express();
 
 const allowedOrigins = [
-    "https://blinkit-client-ten.vercel.app",
-    "https://blinkit-client-ten.vercel.app/",
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5173/"
+    "https://blinkit-client-ten.vercel.app"
 ];
 
 app.use(cors({
